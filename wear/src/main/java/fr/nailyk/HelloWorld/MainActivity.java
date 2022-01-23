7126bbeb50f773304dbbcdbce9d36677391fd8c6
nailyk-fr <git@nailyk.fr>
2022-01-23 19:01:10 +0100

    Initial commit
    Signed-off-by: nailyk-fr <git@nailyk.fr>

---
+++ wear/src/main/java/fr/nailyk/HelloWorld/MainActivity.java
@@ -0,0 +1,23 @@
+package fr.nailyk.HelloWorld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import fr.nailyk.HelloWorld.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+}
